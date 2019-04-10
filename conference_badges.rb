a0a0a8a73e30586e17dde315a3f8e91854f9061a
Unsar Malik <unsar.malik24@gmail.com>
2019-04-10 03:28:46 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -2,3 +2,11 @@
 def badge_maker(name)
   puts "Hello, my name is #{name}."
 end 
+
+def batch_badge_creator(attendees)
+  badges=[]
+  attendees.each do|badge|
+  badges.push("Hello, my name is #{badge}.")
+  end
+  puts badges
+end 
