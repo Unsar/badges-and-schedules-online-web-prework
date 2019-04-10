56471129d4b6bd30c841d3e65b8dac4c6437a5f1
Unsar Malik <unsar.malik24@gmail.com>
2019-04-10 03:29:53 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -11,3 +11,20 @@ def batch_badge_creator(attendees)
   return badges
 end 
 
+def assign_rooms(speakers)
+  assign=[]
+  speakers.each_with_index{|speaker,index|
+  assign.push("Hello, #{speaker}! You'll be assigned to room #{index + 1}!")
+    }
+   return assign
+end 
+
+def printer(attendees)
+  batch_badge_creator(attendees).each do |badge|
+    puts badge
+  end
+  
+  assign_rooms(attendees).each do |badge|
+    puts badge
+  end
+end 
